8702f1283b91246bcc8fec40a19ed89a91e79161
emredemir <aemdemir@gmail.com>
2018-12-10 01:10:11 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# micro-multiplication
+
+A sample application in order to experience Microservices Architecture.
+
+Includes:
+- TDD
+- Eureka
+- Ribbon
+- Zuul
+- Cucumber
+
+Following the book on [Amazon](https://www.amazon.com/Learn-Microservices-Spring-Boot-Practical/dp/1484231643)
---
+++ src/main/java/microservices/book/multiplication/MicroMultiplicationApplication.java
@@ -0,0 +1,12 @@
+package microservices.book.multiplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroMultiplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroMultiplicationApplication.class, args);
+	}
+}
