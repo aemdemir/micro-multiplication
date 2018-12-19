d241839004e1e10989b02fa16d06e7e5b62676f8
emredemir <aemdemir@gmail.com>
2018-12-19 03:43:17 +0300

    v4:
    * Persist users' correct and wrong attempts
    * Show users' attempts on frontend

--- src/main/java/microservices/book/multiplication/service/MultiplicationService.java
+++ src/main/java/microservices/book/multiplication/service/MultiplicationService.java
@@ -3,6 +3,8 @@ package microservices.book.multiplication.service;
 import microservices.book.multiplication.domain.Multiplication;
 import microservices.book.multiplication.domain.MultiplicationResultAttempt;
 
+import java.util.List;
+
 public interface MultiplicationService {
 
     /**
@@ -16,4 +18,12 @@ public interface MultiplicationService {
      * @return true if the attempt matches the result of the multiplication, false otherwise.
      */
     boolean checkAttempt(final MultiplicationResultAttempt resultAttempt);
+
+    /**
+     * Gets the statistics for a given user.
+     *
+     * @param userAlias the user's alias
+     * @return a list of {@link MultiplicationResultAttempt} objects, being the past attempts of the user.
+     */
+    List<MultiplicationResultAttempt> getStatsForUser(final String userAlias);
 }
--- src/main/java/microservices/book/multiplication/service/MultiplicationServiceImpl.java
+++ src/main/java/microservices/book/multiplication/service/MultiplicationServiceImpl.java
@@ -2,18 +2,32 @@ package microservices.book.multiplication.service;
 
 import microservices.book.multiplication.domain.Multiplication;
 import microservices.book.multiplication.domain.MultiplicationResultAttempt;
+import microservices.book.multiplication.domain.User;
+import microservices.book.multiplication.repository.MultiplicationRepository;
+import microservices.book.multiplication.repository.MultiplicationResultAttemptRepository;
+import microservices.book.multiplication.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MultiplicationServiceImpl implements MultiplicationService {
 
     private RandomGeneratorService randomGeneratorService;
+    private MultiplicationResultAttemptRepository attemptRepository;
+    private UserRepository userRepository;
 
     @Autowired
-    public MultiplicationServiceImpl(RandomGeneratorService randomGeneratorService) {
+    public MultiplicationServiceImpl(final RandomGeneratorService randomGeneratorService,
+                                     final MultiplicationResultAttemptRepository attemptRepository,
+                                     final UserRepository userRepository) {
         this.randomGeneratorService = randomGeneratorService;
+        this.attemptRepository = attemptRepository;
+        this.userRepository = userRepository;
     }
 
     @Override
@@ -23,25 +37,38 @@ public class MultiplicationServiceImpl implements MultiplicationService {
         return new Multiplication(factorA, factorB);
     }
 
+    @Transactional
     @Override
     public boolean checkAttempt(final MultiplicationResultAttempt attempt) {
+        // Check if the user already exists for that alias
+        Optional<User> user = userRepository.findByAlias(attempt.getUser().getAlias());
+
+        // Avoids 'hack' attempts
+        Assert.isTrue(!attempt.isCorrect(), "You can't send an attempt marked as correct!");
+
         // Checks if it's correct
         boolean correct = attempt.getResultAttempt() ==
                                 attempt.getMultiplication().getFactorA() *
                                 attempt.getMultiplication().getFactorB();
 
-        // Avoids 'hack' attempts
-        Assert.isTrue(!attempt.isCorrect(), "You can't send an attempt marked as correct!");
 
         // Creates a copy, now setting the 'correct' field accordingly
         MultiplicationResultAttempt checkedAttempt =
                 new MultiplicationResultAttempt(
-                        attempt.getUser(),
+                        user.orElse(attempt.getUser()),
                         attempt.getMultiplication(),
                         attempt.getResultAttempt(),
                         correct);
 
+        // Stores the attempt
+        attemptRepository.save(checkedAttempt);
+
         // Returns the result
         return correct;
+    }
+
+    @Override
+    public List<MultiplicationResultAttempt> getStatsForUser(String userAlias) {
+        return attemptRepository.findTop5ByUserAliasOrderByIdDesc(userAlias);
     }
 }
